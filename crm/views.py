58ae28e2cca238b0d7382aa744ce2ad1b577abbc
DenisFenix <phoenixstudio24@gmail.com>
2021-03-09 17:26:40 +0300

    Обработка ошибок и исключений

--- crm/views.py
+++ crm/views.py
@@ -43,9 +43,12 @@ def thanks_page(request):
     В класс Order добавляются два значения, затем создается экземпляр класса,
     после чего происходит сохранение в базу данных и отправление в шаблон thanks_page.html
     """
-    name = request.POST['name']
-    phone = request.POST['phone']
-    element = Order(order_name=name, order_phone=phone)
-    element.save()
-    sendTelegram(tg_name=name, tg_phone=phone)
-    return render(request, './thanks.html', {'name': name, })
+    if request.POST:
+        name = request.POST['name']
+        phone = request.POST['phone']
+        element = Order(order_name=name, order_phone=phone)
+        element.save()
+        sendTelegram(tg_name=name, tg_phone=phone)
+        return render(request, './thanks.html', {'name': name, })
+    else:
+        return render(request, './thanks.html')
